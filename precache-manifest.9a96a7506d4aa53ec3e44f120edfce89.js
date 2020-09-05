6b16742a045321e1e86f48da2e4fb7a8e8c078d7
Maksim Skipin <skipysmaks@gmail.com>
2020-09-05 21:38:02 +0300

    Updates

---
+++ precache-manifest.9a96a7506d4aa53ec3e44f120edfce89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d230e4027953ad33057a297117af2002",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "f8b1cd174ebd6e76d524",
+    "url": "/goit-react-hw-02-feedback/static/css/2.3d72dcb8.chunk.css"
+  },
+  {
+    "revision": "7a74c1ab5dc4b7918d82",
+    "url": "/goit-react-hw-02-feedback/static/css/main.294605f7.chunk.css"
+  },
+  {
+    "revision": "f8b1cd174ebd6e76d524",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8877d103.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.8877d103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a74c1ab5dc4b7918d82",
+    "url": "/goit-react-hw-02-feedback/static/js/main.fa7cb007.chunk.js"
+  },
+  {
+    "revision": "b58a435a92ac135c88f5",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.82cfee65.js"
+  }
+]);
